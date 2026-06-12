fdd8bc6408ad38d1fcaec101c2cc1e7911d2b1de
Hafsa <hafsahounaoui@gmail.com>
2026-06-12 15:01:51 +0100

    update ?

--- frontend/src/app/ecole/gestion-eleves/gestion-eleves.ts
+++ frontend/src/app/ecole/gestion-eleves/gestion-eleves.ts
@@ -1,4 +1,4 @@
-import { Component, afterNextRender, ChangeDetectorRef } from '@angular/core'; // ⚡ Ajout de ChangeDetectorRef
+import { Component, afterNextRender, ChangeDetectorRef } from '@angular/core'; 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
